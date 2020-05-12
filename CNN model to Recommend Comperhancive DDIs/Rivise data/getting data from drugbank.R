f842a1bffbbfe7792f3351d5a6e6490de4af956b
Mohammad Amin Khodamoradi <38489600+aminkhod@users.noreply.github.com>
2020-05-12 21:33:47 +0430

    12th May

--- CNN model to Recommend Comperhancive DDIs/Rivise data/getting data from drugbank.R
+++ CNN model to Recommend Comperhancive DDIs/Rivise data/getting data from drugbank.R
@@ -1,11 +1,38 @@
 install.packages("devtools")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("AnnotationDbi")
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("org.Hs.eg.db")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("org.Hs.eg.db")
+
 devtools::install_github("yduan004/drugbankR")
 
 
 library(drugbankR)
 
-drugbank_dataframe <- dbxml2df(xmlfile="drugbank.xml") 
-df2SQLite(dbdf=drugbank_dataframe, version="5.1.3")
+drugbank_dataframe <- dbxml2df(xmlfile="drugbank.xml")
+# , version="5.1.3"
+df2SQLite(dbdf=drugbank_dataframe)
+# , version="5.1.3"
+
+install.packages("openxlsx", dependencies=TRUE)
+
+library(openxlsx)
+
+# # read data from an Excel file or Workbook object into a data.frame
+# df <- read.xlsx('name-of-your-excel-file.xlsx')
+
+# for writing a data.frame or list of data.frames to an xlsx file
+write.xlsx(drugbank_dataframe, 'drugbank.xlsx')
+
+
 
 all <- queryDB(type = "getAll", db_path="drugbank_5.1.3.db") # get the entire drugbank dataframe
 dim(all)
